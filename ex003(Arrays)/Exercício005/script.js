221e4f78f460567fda8b821aba87060e34d36acf
Everson Queiroz <everson_queiroz7@hotmail.com>
2020-09-17 22:31:27 -0300

    em andamento
    Exercício em andamento...

--- ex003(Arrays)/Exercício005/script.js
+++ ex003(Arrays)/Exercício005/script.js
@@ -1,27 +1,46 @@
-function adicionar(){
-    let num = document.getElementById("numid")
-    let tab = document.getElementById("selid")
+let num = document.getElementById("numid")
+let lista = document.getElementById("selid")
+let res = document.querySelector('div#res')
+let valores = []
 
-    if(num.value.length == 0 || num.value > 100){
-        alert("Digite um número válido")
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     }else{
-        let item = document.createElement('option')
-        item.value = `tab${num.value}`
-        item.text = `Valor ${num.value} adicionado`
-        tab.appendChild(item)
+        return false
     }
 }
 
-function finalizar(){
-    let res = document.getElementById("res")
-    vetnum.sort()
-
-    for(c = 0; c <= vetnum.length; c++){
-        soma += vetnum[c]
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
     }
-    res.innerHTML = `Ao todo, temos ${vetnum.length} números cadastrados`
-    res.innerHTML += `O maior valor informado foi ${vetnum.length - 1}`
-    res.innerHTML += `O menor valor informado foi ${vetnum[0]}`
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        let item = document.createElement('option')
+        item.value = `lista${num.value}`
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        valores.push(num.value)
+    }else{
+        alert("Valor Inválido ou já encontrado na lista")
+    }
+}
+2
+function finalizar(){
+    valores.sort()
+    alert(`é sla, isso aqui funcionou ${valores}`)
+   /* valores.sort()
+   for(c = 0; c <= valores.length; c++){
+        soma += valores[c]
+    }
+    res.innerHTML = `Ao todo, temos ${valores.length} números cadastrados`
+    res.innerHTML += `O maior valor informado foi ${valores[valores.length - 1]}`
+    res.innerHTML += `O menor valor informado foi ${valores[0]}`
     res.innerHTML += `Somando todos os valores temos: ${soma}`
-    res.innerHTML += `A média dos valores digitados é: ${soma / vetnum.length}`
+    res.innerHTML += `A média dos valores digitados é: ${soma / valores.length}`*/
 }
